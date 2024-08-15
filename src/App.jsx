0a269c0da3817fb00adc3eaaf66aa082b12266d4
unknown <shokirjonovokifxon46@gmail.com>
2024-08-15 12:12:07 +0500

    vazifa

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,68 @@
-import React from 'react';
-import Cardone from './components/cardone';
-import Cardtwo from './components/cardtwo';
-import Cardthree from './components/cardthree';
-import '../src/App.css'
-import { Routes, Route, useNavigate } from 'react-router-dom';
-  function App(){
-   const Navigate = useNavigate('')
-   function one(e){
-    e.preventDefault()
-    Navigate('/')
-   }
-   function two(e){
-    e.preventDefault()
-    Navigate('/cardtwo')
-   }
-   function three(e){
-    e.preventDefault()
-    Navigate('/cardthree')
-   }
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+    const [page, setPage] = useState(1);  
+    const [prod, setProd] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/all?_start=${(page - 1) * 1}&_limit=10`)  
+            .then(resp => resp.json())
+            .then(data => {
+                setProd(data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, [page]);  
+
+    function one(e) {
+        e.preventDefault();
+        console.log(e.target.value);
+        setPage(Number(e.target.value));  
+    }
+
     return (
-      <div>
-        <Routes>
-          <Route path='/' element={<Cardone></Cardone>}></Route>
-          <Route path='/cardtwo' element={<Cardtwo></Cardtwo>}></Route>
-          <Route path='/cardthree' element={<Cardthree></Cardthree>}></Route>
-        </Routes>
+        <div>
+            <div className="container">
+                <br />
+                <br />
+               <div className="prod container">
+               {prod.map((item, index) => (
+                    <div className="carrd" key={index}>
+                        <img src="https://picsum.photos/200/300" alt="" />
+                        <h1>{item.country}</h1>
+                        <p>{item.code} <span>{item.id}</span></p>
+                    </div>
+                ))} 
+                </div><br />
+                <div className="but">
+                    <button onClick={one} value={1}>1</button>
+                    <button onClick={one} value={11}>2</button>
+                    <button onClick={one} value={21}>3</button>
+                    <button onClick={one} value={31}>4</button>
+                    <button onClick={one} value={41}>5</button><br />
+                    <button onClick={one} value={51}>6</button>
+                    <button onClick={one} value={61}>7</button>
+                    <button onClick={one} value={71}>8</button>
+                    <button onClick={one} value={81}>9</button>
+                    <button onClick={one} value={91}>10</button><br />
+                    <button onClick={one} value={101}>11</button>
+                    <button onClick={one} value={111}>12</button>
+                    <button onClick={one} value={121}>13</button>
+                    <button onClick={one} value={131}>14</button>
+                    <button onClick={one} value={141}>15</button><br />
+                    <button onClick={one} value={151}>16</button>
+                    <button onClick={one} value={161}>17</button>
+                    <button onClick={one} value={171}>18</button>
+                    <button onClick={one} value={181}>19</button>
+                    <button onClick={one} value={191}>20</button>
+                </div>
+            </div>
 
-       <div className="container">
-      <div className="but">
-      <button onClick={one}>1</button>
-        <button onClick={two}>2</button>
-        <button onClick={three}>3</button>
-      </div>
-       </div>
-      </div>
-
-      
-
-
-
-
+           
+        </div>
     );
-  }
-export default App
+}
+
+export default App;
